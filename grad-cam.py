4d944411b2dc03927886463971622561ddb2a484
himalayanZephyr <42401082+himalayanZephyr@users.noreply.github.com>
2019-04-13 19:22:06 +0200

    changing deprecated retain_variables
    changed 'retain_variables' which is deprecated to 'retain_graph'

--- grad-cam.py
+++ grad-cam.py
@@ -101,7 +101,7 @@ class GradCam:
 
 		self.model.features.zero_grad()
 		self.model.classifier.zero_grad()
-		one_hot.backward(retain_variables=True)
+		one_hot.backward(retain_graph=True)
 
 		grads_val = self.extractor.get_gradients()[-1].cpu().data.numpy()
 
@@ -173,7 +173,7 @@ class GuidedBackpropReLUModel:
 
 		# self.model.features.zero_grad()
 		# self.model.classifier.zero_grad()
-		one_hot.backward(retain_variables=True)
+		one_hot.backward(retain_graph=True)
 
 		output = input.grad.cpu().data.numpy()
 		output = output[0,:,:,:]
