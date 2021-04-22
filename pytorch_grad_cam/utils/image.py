e3fbaa41d9a45c57efbdc33544c1bf9cb1eb6fd6
LucaButera <22855332+LucaButera@users.noreply.github.com>
2021-04-22 19:37:09 +0200

    Added optional conversion to rgb in cam heatmap overlay generation

--- pytorch_grad_cam/utils/image.py
+++ pytorch_grad_cam/utils/image.py
@@ -28,8 +28,10 @@ def deprocess_image(img):
     return np.uint8(img * 255)
 
 
-def show_cam_on_image(img: np.ndarray, mask: np.ndarray) -> np.ndarray:
+def show_cam_on_image(img: np.ndarray, mask: np.ndarray, return_rgb: bool = False) -> np.ndarray:
     heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
+    if return_rgb:
+        heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
     heatmap = np.float32(heatmap) / 255
     cam = heatmap + np.float32(img)
     cam = cam / np.max(cam)
