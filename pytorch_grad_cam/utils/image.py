582913a34264a45b581d23d13d0b42351ffef3a4
LucaButera <22855332+LucaButera@users.noreply.github.com>
2021-04-22 19:53:26 +0200

    Renamed flag parameter and added customizable colormap

--- pytorch_grad_cam/utils/image.py
+++ pytorch_grad_cam/utils/image.py
@@ -28,9 +28,18 @@ def deprocess_image(img):
     return np.uint8(img * 255)
 
 
-def show_cam_on_image(img: np.ndarray, mask: np.ndarray, return_rgb: bool = False) -> np.ndarray:
-    heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
-    if return_rgb:
+def show_cam_on_image(img: np.ndarray, mask: np.ndarray, use_rgb: bool = False, colormap: int = cv2.COLORMAP_JET) -> np.ndarray:
+    """ This function overlays the cam mask on the image as an heatmap.
+    By default the heatmap is in BGR format.
+
+    :param img: The base image in RGB or BGR format.
+    :param mask: The cam mask.
+    :param use_rgb: Whether to use an RGB or BGR heatmap, this should be set to True if 'img' is in RGB format.
+    :param colormap: The OpenCV colormap to be used.
+    :returns: The default image with the cam overlay.
+    """
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), colormap)
+    if use_rgb:
         heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
     heatmap = np.float32(heatmap) / 255
     cam = heatmap + np.float32(img)
