4621ab466b34f802c4a1e20a0f35a115cf103738
Jacob Gildenblat <jacob.gildenblat@gmail.com>
2021-12-21 15:24:32 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 with open('README.md', mode='r', encoding='utf-8') as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as f:
+    requirements = f.readlines()
+
 setuptools.setup(
     name='grad-cam',
-    version='1.3.5',
+    version='1.3.6',
     author='Jacob Gildenblat',
     author_email='jacob.gildenblat@gmail.com',
     description='Many Class Activation Map methods implemented in Pytorch. '
@@ -23,11 +26,5 @@ setuptools.setup(
     ],
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
-    install_requires=[
-        'opencv-python>=4.1'
-        'torch>=1.4',
-        'torchvision>=0.5',
-        'ttach>=0.0.3',
-        'tqdm>=4.42'
-    ]
+    install_requires=requirements
 )
