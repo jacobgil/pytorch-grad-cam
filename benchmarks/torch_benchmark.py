15ca2becdb16c7c67f4c32abc174285d25fbc6aa
trex22 <contact@jasonchalom.com>
2022-11-06 11:47:57 +0200

    Refactor code to share some algorithm :cop:

--- benchmarks/torch_benchmark.py
+++ benchmarks/torch_benchmark.py
@@ -19,9 +19,52 @@ from torchvision import models
 
 from torch.profiler import profile, record_function, ProfilerActivity
 
+def run_gradcam(model, number_of_inputs, use_cuda=False):
+    min_time = 10000000000000
+    max_time = 0
+    sum_of_times = 0
+
+    dev = torch.device('cpu')
+    if use_cuda:
+        dev = torch.device('cuda:0')
+
+    # TODO: Use real data?
+    # TODO: Configurable dimensions?
+
+    # Some defaults I use in research code
+    input_tensor = torch.rand((number_of_inputs, 3, 256, 60))
+    batch_size = 8
+    targets = None # [ClassifierOutputTarget(None)]
+
+    model.to(dev)
+    target_layers = [model.layer4] # Last CNN layer of ResNet50
+
+    cam_function = GradCAM(model=model, target_layers=target_layers, use_cuda=use_cuda)
+    cam_function.batch_size = batch_size
+
+    for i in range(number_of_inputs):
+        start_time = time.time()
+
+        # Actual code to benchmark
+        input_image = input_tensor[i].to(dev)
+        heatmap = cam_function(input_tensor=input_image, targets=targets)
+
+        end_time = time.time()
+        time_difference = end_time - start_time
+
+        sum_of_times += time_difference
+
+        if time_difference > max_time:
+            max_time = time_difference
+
+        if time_difference < min_time:
+            min_time = time_difference
+
+    avg_time = sum_of_times / number_of_inputs
+    return [min_time, max_time, avg_time]
+
 number_of_inputs = 1000
 model =  models.resnet50()
-input_tensor = torch.rand((number_of_inputs, 3, 256, 60)) # TODO: Use real data?
 
 # TODOs:
 # Test with numpy v1.4.6 (master)
@@ -30,75 +73,19 @@ input_tensor = torch.rand((number_of_inputs, 3, 256, 60)) # TODO: Use real data?
 # Nice output
 
 # Run on CPU with profiler (save the profile to print later)
-dev = torch.device('cpu')
-use_cuda = False
-
-model.to(dev)
-input_tensor.to(dev)
-
-# Some defaults I use in research code
-target_layers = [model.layer4]
-batch_size = 8
-targets = None # [ClassifierOutputTarget(None)]
-
-# Profile the CPU call
 with profile(activities=[ProfilerActivity.CPU], profile_memory=True, record_shapes=True) as prof:
-    cam_function = GradCAM(model=model, target_layers=target_layers, use_cuda=use_cuda)
-    cam_function.batch_size = batch_size
-    heatmap = cam_function(input_tensor=input_tensor, targets=targets)
-
+    cpu_profile_min_time, cpu_profile_max_time, cpu_profile_avg_time = run_gradcam(model, number_of_inputs, use_cuda=False)
 cpu_profile = prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=15)
 
 # Run on CUDA with profiler (save the profile to print later)
-dev = torch.device('cuda')
-use_cuda = True
-
-model.to(dev)
-input_tensor.to(dev)
-
 with profile(activities=[ProfilerActivity.CPU], profile_memory=True, record_shapes=True) as prof:
-    cam_function = GradCAM(model=model, target_layers=target_layers, use_cuda=use_cuda)
-    cam_function.batch_size = batch_size
-    heatmap = cam_function(input_tensor=input_tensor, targets=targets)
-
+    cuda_profile_min_time, cuda_profile_max_time, cuda_profile_avg_time = run_gradcam(model, number_of_inputs, use_cuda=True)
 cuda_profile = prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=15)
 
 # Run on CPU x1000 (get min, max, and avg times)
-cpu_min_time = 10000000000000
-cpu_max_time = 0
-cpu_sum_of_times = 0
+cpu_min_time, cpu_max_time, cpu_avg_time = run_gradcam(model, number_of_inputs, use_cuda=False)
 
-for i in range(number_of_inputs):
-    start_time = time.time()
-
-    input_tensor = torch.rand((number_of_inputs, 3, 256, 60)) # TODO: Use real data?
-
-    dev = torch.device('cpu')
-    use_cuda = False
-
-    model.to(dev)
-    input_tensor.to(dev)
-
-    # Some defaults I use in research code
-    target_layers = [model.layer4]
-    batch_size = 8
-    targets = None # [ClassifierOutputTarget(None)]
-
-    cam_function = GradCAM(model=model, target_layers=target_layers, use_cuda=use_cuda)
-    cam_function.batch_size = batch_size
-    heatmap = cam_function(input_tensor=input_tensor, targets=targets)
-
-    end_time = time.time()
-    time_difference = end_time - start_time
-
-    cpu_sum_of_times += time_difference
-
-    if time_difference > cpu_max_time:
-        cpu_max_time = time_difference
-
-    if time_difference < cpu_min_time:
-        cpu_min_time = time_difference
-
-cpu_avg_time = cpu_sum_of_times / number_of_inputs
-breakpoint()
 # Run on CUDA x1000
+cuda_min_time, cuda_max_time, cuda_avg_time = run_gradcam(model, number_of_inputs, use_cuda=True)
+
+breakpoint()
