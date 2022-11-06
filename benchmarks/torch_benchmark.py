f4b759ad12b66d4fcece2d38f9fdcb297d96c4ab
trex22 <contact@jasonchalom.com>
2022-11-06 11:40:16 +0200

    Create the large loop :cop:

--- benchmarks/torch_benchmark.py
+++ benchmarks/torch_benchmark.py
@@ -19,13 +19,15 @@ from torchvision import models
 
 from torch.profiler import profile, record_function, ProfilerActivity
 
+number_of_inputs = 1000
 model =  models.resnet50()
-input_tensor = torch.rand((1, 3, 256, 60)) # TODO: Use real data?
+input_tensor = torch.rand((number_of_inputs, 3, 256, 60)) # TODO: Use real data?
 
 # TODOs:
 # Test with numpy v1.4.6 (master)
 # Test with torch v1.4.7 (wip)
 # Test other CAMs besides GradCAM
+# Nice output
 
 # Run on CPU with profiler (save the profile to print later)
 dev = torch.device('cpu')
@@ -61,9 +63,42 @@ with profile(activities=[ProfilerActivity.CPU], profile_memory=True, record_shap
 
 cuda_profile = prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=15)
 
+# Run on CPU x1000 (get min, max, and avg times)
+cpu_min_time = 10000000000000
+cpu_max_time = 0
+cpu_sum_of_times = 0
+
+for i in range(number_of_inputs):
+    start_time = time.time()
+
+    input_tensor = torch.rand((number_of_inputs, 3, 256, 60)) # TODO: Use real data?
+
+    dev = torch.device('cpu')
+    use_cuda = False
+
+    model.to(dev)
+    input_tensor.to(dev)
+
+    # Some defaults I use in research code
+    target_layers = [model.layer4]
+    batch_size = 8
+    targets = None # [ClassifierOutputTarget(None)]
+
+    cam_function = GradCAM(model=model, target_layers=target_layers, use_cuda=use_cuda)
+    cam_function.batch_size = batch_size
+    heatmap = cam_function(input_tensor=input_tensor, targets=targets)
+
+    end_time = time.time()
+    time_difference = end_time - start_time
+
+    cpu_sum_of_times += time_difference
+
+    if time_difference > cpu_max_time:
+        cpu_max_time = time_difference
+
+    if time_difference < cpu_min_time:
+        cpu_min_time = time_difference
+
+cpu_avg_time = cpu_sum_of_times / number_of_inputs
 breakpoint()
-
-# Run on CPU x100 (get min, max, and avg times)
-
-
-# Run on CUDA x100
+# Run on CUDA x1000
