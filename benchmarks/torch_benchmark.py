1a77f74a5867021780b12d971ba627fe5a122b17
trex22 <contact@jasonchalom.com>
2022-11-06 11:36:00 +0200

    Add in cuda profiling :cop:

--- benchmarks/torch_benchmark.py
+++ benchmarks/torch_benchmark.py
@@ -2,6 +2,7 @@ import argparse
 import cv2
 import numpy as np
 import torch
+import time
 
 from pytorch_grad_cam import GradCAM, \
     ScoreCAM, \
@@ -44,11 +45,25 @@ with profile(activities=[ProfilerActivity.CPU], profile_memory=True, record_shap
     cam_function.batch_size = batch_size
     heatmap = cam_function(input_tensor=input_tensor, targets=targets)
 
-print(prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=15))
-breakpoint() # For now as I write this
+cpu_profile = prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=15)
 
 # Run on CUDA with profiler (save the profile to print later)
+dev = torch.device('cuda')
+use_cuda = True
+
+model.to(dev)
+input_tensor.to(dev)
+
+with profile(activities=[ProfilerActivity.CPU], profile_memory=True, record_shapes=True) as prof:
+    cam_function = GradCAM(model=model, target_layers=target_layers, use_cuda=use_cuda)
+    cam_function.batch_size = batch_size
+    heatmap = cam_function(input_tensor=input_tensor, targets=targets)
+
+cuda_profile = prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=15)
+
+breakpoint()
 
 # Run on CPU x100 (get min, max, and avg times)
+
 
 # Run on CUDA x100
