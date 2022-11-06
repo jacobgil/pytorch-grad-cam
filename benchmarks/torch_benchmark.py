02b94515511a7b87ec0d1f30375925a0915f32d9
trex22 <contact@jasonchalom.com>
2022-11-06 11:17:18 +0200

    Create a simple benchmark :cop:

---
+++ benchmarks/torch_benchmark.py
@@ -0,0 +1,40 @@
+import argparse
+import cv2
+import numpy as np
+import torch
+
+from pytorch_grad_cam import GradCAM, \
+    ScoreCAM, \
+    GradCAMPlusPlus, \
+    AblationCAM, \
+    XGradCAM, \
+    EigenCAM, \
+    EigenGradCAM, \
+    LayerCAM, \
+    FullGrad
+
+import torchvision # You may need to install separately
+from torchvision import models
+
+from torch.profiler import profile, record_function, ProfilerActivity
+
+model =  models.resnet50()
+random_tensor = torch.rand((256, 60, 3)) # TODO: Use real data?
+
+# Test with numpy v1.4.6 (master)
+# Test with torch v1.4.7 (wip)
+
+# Run on CPU with profiler (save the profile to print later)
+dev = torch.device('cpu')
+model.to(dev)
+
+with profile(activities=[ProfilerActivity.CPU], profile_memory=True, record_shapes=True) as prof:
+
+print(prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=15))
+breakpoint() # For now as I write this
+
+# Run on CUDA with profiler (save the profile to print later)
+
+# Run on CPU x100 (get min, max, and avg times)
+
+# Run on CUDA x100
