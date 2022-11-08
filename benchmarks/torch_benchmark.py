d492c36262ec2fbf19b76dc30515ee23f3a3c405
trex22 <contact@jasonchalom.com>
2022-11-08 18:19:12 +0200

    Force cuda device :cop:

--- benchmarks/torch_benchmark.py
+++ benchmarks/torch_benchmark.py
@@ -59,7 +59,7 @@ def run_gradcam(model, number_of_inputs, batch_size=8, use_cuda=False, workflow_
         if workflow_test:
             for j in range(batch_size):
                 # Create a binary map
-                threshold_plot = torch.where(torch.tensor(heatmap[j]) > 0.5, 1, 0)
+                threshold_plot = torch.where(torch.tensor(heatmap[j]).to(torch.device('cuda:0')) > 0.5, 1, 0)
                 output_image = input_image * threshold_plot
 
         end_time = time.time()
