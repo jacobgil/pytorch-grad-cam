273bd81ecdebda2618177cc7f1a7fed7372d28aa
trex22 <contact@jasonchalom.com>
2023-04-26 18:20:00 +0200

    Make more meaningful changes extracted from another branch :cop:

--- pytorch_grad_cam/utils/svd_on_activations.py
+++ pytorch_grad_cam/utils/svd_on_activations.py
@@ -1,9 +1,9 @@
-import numpy as np
+import torch
 
 
 def get_2d_projection(activation_batch):
     # TBD: use pytorch batch svd implementation
-    activation_batch[np.isnan(activation_batch)] = 0
+    activation_batch[torch.isnan(activation_batch)] = 0
     projections = []
     for activations in activation_batch:
         reshaped_activations = (activations).reshape(
@@ -12,8 +12,8 @@ def get_2d_projection(activation_batch):
         # Otherwise the image returned is negative
         reshaped_activations = reshaped_activations - \
             reshaped_activations.mean(axis=0)
-        U, S, VT = np.linalg.svd(reshaped_activations, full_matrices=True)
+        U, S, VT = torch.linalg.svd(reshaped_activations, full_matrices=True)
         projection = reshaped_activations @ VT[0, :]
         projection = projection.reshape(activations.shape[1:])
         projections.append(projection)
-    return np.float32(projections)
+    return torch.tensor(projections).to(torch.float32)
