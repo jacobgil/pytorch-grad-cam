2d1c09fd476a3209ced62e565d2af71239a6487b
Sachin Karmani <sachin.karmani52@gmail.com>
2024-10-02 17:11:53 -0400

    Create kpca_cam.py

---
+++ pytorch_grad_cam/kpca_cam.py
@@ -0,0 +1,20 @@
+from pytorch_grad_cam.base_cam import BaseCAM
+from pytorch_grad_cam.utils.svd_on_activations import get_2d_projection_kernel
+
+class KPCA_CAM(BaseCAM):
+    def __init__(self, model, target_layers, 
+                 reshape_transform=None, kernel='sigmoid', gamma=None):
+        super(KPCA_CAM, self).__init__(model,
+                                       target_layers,
+                                       reshape_transform,
+                                       uses_gradients=False,
+                                       kernel=kernel, gamma=gamma)
+
+    def get_cam_image(self,
+                      input_tensor,
+                      target_layer,
+                      target_category,
+                      activations,
+                      grads,
+                      eigen_smooth):
+        return get_2d_projection_kernel(activations, self.kernel, self.gamma)
